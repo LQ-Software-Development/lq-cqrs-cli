5c3cd6b471f5fd40266311aeb615d235492ffba9
QuadriniL <quadrinho.tiolu@gmail.com>
2023-01-08 21:31:55 -0300

    fix: remove dto folder

--- dto/request/CreateUserRequestDTO.ts
+++
@@ -1,9 +0,0 @@
-import { ICreateUserUseCase, request, result } from './ICreateUserUseCase';
-
-export class CreateUserUseCase implements ICreateUserUseCase {
-  constructor() {}
-
-  async execute(data: request): Promise<result> {
-    return Result.ok({});
-  }
-}
